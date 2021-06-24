e10dbaf11207d82435f250ace9da4f15d798b263
shotaro tanaka <5511068t@gmail.com>
2021-06-25 00:31:56 +0900

    210625

--- cpp/06/12.cpp
+++ cpp/06/12.cpp
@@ -1,5 +1,6 @@
 #include <iostream> // cout, endl, cin
 #include <vector> 
+#include <cstdlib>
 using namespace std;
 
 #define rep(i,n) for (int i = 0; i < (int)(n); ++i)
@@ -16,8 +17,16 @@ int main() {
     a[i] = l;
   }
 
+  int cnt = 0;
+
   rep(i, N) {
-    cout << a[i] << endl;
+    if(abs(0-a[i]) <= abs(K-a[i])) {
+      cnt += 2 * abs(0-a[i]);
+    } else {
+      cnt += 2 * abs(K-a[i]);
+    }
   }
+
+  cout << cnt << endl;
 
 }
