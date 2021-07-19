91db7d3108915b177a6289682eea644709ab05f8
jacoloves <5511068t@gmail.com>
2021-07-19 18:45:21 +0900

    210719

--- cpp/07/14.cpp
+++ cpp/07/14.cpp
@@ -9,5 +9,19 @@ using namespace std;
 #define rep(i,n) for (int i = 0; i < (int)(n); ++i)
 
 int main() {
+  int n;
+  cin >> n;
+
+  vector<int> v(n);
+
+  rep(i, n) {
+    cin >> v.at(i);
+  }
+
+
+  sort(v.begin(), v.end());
+  v.erase(unique(v.begin(), v.end()), v.end());
+
+  cout << v.size() << endl;
 }
 
