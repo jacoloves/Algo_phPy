9403e7ca200622af2d4d8dc7c8c38e1e5ad023b1
jacoloves <5511068t@gmail.com>
2021-08-03 22:23:50 +0900

    210803

--- cpp/08/2.cpp
+++ cpp/08/2.cpp
@@ -8,6 +8,26 @@ using namespace std;
 
 #define rep(i,n) for (int i = 0; i < (int)(n); ++i)
 
+int verif_func(vector<vector<int>> &a) {
+  bool s_flg = false;
+  int ex_cnt = 0;
+  rep(i, 10){
+    rep(j, 10){
+      if(a.at(i).at(j) == 1)
+        s_flg = true;
+
+      if(s_flg){
+        if(i == 0){
+          if(j == 0){
+            if ((a.at(i).at(j+1) == 1 || a.at(i+1).at(j) == 1) && a.at(i).at(j) == 1)
+              ex_cnt == 0;
+          }else if(j == 9)
+        }
+      }
+    }
+  }
+}
+
 int main() {
   vector<vector<char>> data(10, vector<char>(10));
   vector<vector<int>> exch(10, vector<int>(10));
@@ -18,8 +38,6 @@ int main() {
     }
   }
 
-  cout << "-------------" << endl;
-
   rep(i, 10){
     rep(j, 10) {
       if (data.at(i).at(j) == 'o')
@@ -29,6 +47,5 @@ int main() {
     }
   }
   
-  rep(i, 10)
 }
 
