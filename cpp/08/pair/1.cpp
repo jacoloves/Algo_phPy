70fa8ae6acc796294eda1afb163af0a2cfd2c61f
jacoloves <5511068t@gmail.com>
2021-08-16 23:57:23 +0900

    210816

---
+++ cpp/08/pair/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+
+int main() {
+    pair<string, int> p("abc", 3);
+    cout << p.first << endl;
+
+    p.first = "hello";
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    p = make_pair("*", 1);
+
+    string s;
+    int a;
+    tie(s, a) = p;
+    cout << s << endl;
+    cout << a << endl;
+}
+
--- cpp/08/tmp.cpp
+++ cpp/08/tmp.cpp
@@ -1,11 +1,21 @@
 #include <iostream> // cout, endl, cin
-#include <vector> 
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <algorithm>
-#include <bitset>
-#include <iomanip>
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
 using namespace std;
 typedef long long ll;
 
