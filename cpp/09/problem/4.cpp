7ad560364de0092e9b0df0b89e345da1c28d46bc
jacoloves <5511068t@gmail.com>
2021-09-16 13:57:44 +0900

    210916

---
+++ cpp/09/problem/4.cpp
@@ -0,0 +1,40 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == b && b == c) {
+        cout << a << endl;
+    } else if (a == b) {
+        cout << c << endl;
+    } else if (a == c) {
+        cout << b << endl;
+    } else if (b == c) {
+        cout << a << endl;
+    } else {
+        cout << 0 << endl;
+    }
+}
+
---
+++ cpp/09/problem/5.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    int ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        int trans_i = (i+1)*100;
+        for (int j = 0; j<K; j++) {
+            ans += (trans_i + (j+1));
+        }
+    }
+
+    cout << ans << endl;
+}
+
