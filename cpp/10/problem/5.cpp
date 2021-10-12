9e564a938132118a2e46da2775675bb1436a3536
jacoloves <5511068t@gmail.com>
2021-10-12 18:31:01 +0900

    211012

---
+++ cpp/10/problem/5.cpp
@@ -0,0 +1,50 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+
+int factorial_calc(int n) {
+    int ret_num = 1;
+    for (int i=1; i<=n; i++){
+        ret_num *= i;
+    }
+
+    return ret_num;
+}
+
+int main() {
+    int p;
+    cin >> p;
+
+    int max;
+    int fact;
+    int f_cnt = 1;
+
+    while(true) {
+        fact = factorial_calc(f_cnt);
+        if (fact < p)
+            break;
+        f_cnt++;
+    }
+
+    
+}
+
