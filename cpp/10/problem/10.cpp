8d7341348ce7473d45a37586d38a30cc13715464
jacoloves <5511068t@gmail.com>
2021-10-20 18:12:40 +0900

    211020

---
+++ cpp/10/problem/10.cpp
@@ -0,0 +1,45 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string line;
+
+    cin >> line;
+    int cnt = 1;
+
+    rep(i, n) {
+        if (line[i] == '1' && cnt % 2 == 1) {
+            cout << "Takahashi" << endl;
+            break;
+        } else if (line[i] == '1' && cnt % 2 == 0) {
+            cout << "Aoki" << endl;
+            break;
+        }
+
+        cnt++;
+    }
+}
+
