05b275aecce50dfc126d5ae89425d6a367a5df6b
jacoloves <5511068t@gmail.com>
2021-10-22 11:38:59 +0900

    211022

--- cpp/10/problem/11.cpp
+++ cpp/10/problem/11.cpp
@@ -21,7 +21,19 @@ typedef long long ll;
 #define rep(i,n) for (int i = 0; i < (int)(n); ++i)
 using P = pair<int, int>;
 
-int main() {
+const int mod = 1000000007;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    rep(i, n) cin >> c[i];
+    sort(c.begin(), c.end());
+    int ans = 1;
+    rep(i, n) {
+        ans = ((ll)ans * max(c[i]-i, 0)) % mod;
+    }
+    cout << ans << endl;
     return 0;
 }
+
