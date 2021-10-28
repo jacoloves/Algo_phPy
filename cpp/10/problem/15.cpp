232b63bd1efc6df6e55ce19bad7c319627e6814c
jacoloves <5511068t@gmail.com>
2021-10-29 00:01:55 +0900

    211029

--- cpp/10/problem/15.cpp
+++ cpp/10/problem/15.cpp
@@ -24,10 +24,20 @@ using P = pair<int, int>;
 const int mod = 1000000007;
 
 int main() {
-    string str;
-    cin >> str;
-
-    
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector dp(n+1, vector<int>(9));
+    rep(i, n+1) dp[i][0] = 1;
+    string t = "chokudai";
+    rep(i,n)rep(j,8) {
+        if (s[i] != t[j]) {
+            dp[i+1][j+1] = dp[i][j+1];
+        } else {
+            dp[i+1][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
+        }
+    }
+    cout << dp[n][8] << endl;
 
     return 0;
 }
