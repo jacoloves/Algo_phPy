fd4fde90f94dd62f05dbb504effe259072da8583
jacoloves <5511068t@gmail.com>
2021-12-09 13:35:13 +0900

    day1

---
+++ cpp/12/problem/4.cpp
@@ -0,0 +1,37 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+  int a;
+  cin >> a;
+
+  int h,m,s;
+
+  h = a / 3600;
+  a = a % 3600;
+  m = a / 60;
+  s = a % 60;
+  
+  cout << h << ":" << m << ":" << s << endl; 
+    return 0;
+}
