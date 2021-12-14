48e3f878291190d897cf63f19b6aea21f9b1c3ac
jacoloves <5511068t@gmail.com>
2021-12-14 21:04:21 +0900

    day6

---
+++ cpp/12/problem/9.cpp
@@ -0,0 +1,36 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+const double PI = 3.141592653589;
+
+int main() {
+    double n;
+    cin >> n;
+
+    double area = (double)(PI * n * n);
+    double circle = (double)2 * PI * n;
+
+    printf("%.6lf %.6lf\n", area, circle);
+
+    return 0;
+}
