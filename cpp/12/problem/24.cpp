4cbd46fe463d72952c18ab66c10bea7e734d7266
jacoloves <5511068t@gmail.com>
2021-12-26 23:00:26 +0900

    211226

---
+++ cpp/12/problem/24.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    string str, ans;
+
+    getline(cin, str);
+
+    for (int i=0; i < str.length(); i++){
+        //cout << str[i] << endl;
+        if(isupper(str[i])) ans += tolower(str[i]);
+        else if(islower(str[i])) ans += toupper(str[i]);
+        else ans += str[i];
+    }
+    
+    cout << ans << endl;
+
+    return 0;
+}
