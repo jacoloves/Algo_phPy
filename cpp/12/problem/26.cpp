6ee36280c38dbca69f0b84b31c754f59ef394373
jacoloves <5511068t@gmail.com>
2021-12-29 22:05:23 +0900

    211229

---
+++ cpp/12/problem/26.cpp
@@ -0,0 +1,42 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+
+    int ans[25]; 
+    char ans_char[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    char ch;
+
+    while(cin >> ch) {
+        int num = tolower(ch) - 'a';
+        ans[num]++;
+    }
+
+    for (int i=0; i<25; i++) {
+        cout << ans_char[i] << " : " << ans[i] << endl;
+    }
+
+
+    return 0;
+}
