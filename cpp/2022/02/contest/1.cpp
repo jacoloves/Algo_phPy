bc2d19f8265a5cdd2b16da71d5a3abb600ee0033
jacoloves <5511068t@gmail.com>
2022-02-07 01:12:34 +0900

    220207

---
+++ cpp/2022/02/contest/1.cpp
@@ -0,0 +1,49 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll a, b;
+    a = 1;
+    b = pow(n, 2);
+
+    bool flg = false;
+
+    for (int i=0; i<n; i++) {
+        a *= 2;
+        if (a > b) {
+            flg = true;
+            break;
+        }
+    }
+
+    if (flg) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
