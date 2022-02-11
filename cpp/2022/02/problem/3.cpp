ba692e4b2f8b2105129439b49274436aeece65e8
jacoloves <5511068t@gmail.com>
2022-02-11 10:25:11 +0900

    20210211

---
+++ cpp/2022/02/problem/3.cpp
@@ -0,0 +1,53 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    string x;
+    int n;
+    cin >> x >> n;
+    vector<string> s(n);
+    for (int i=0; i<n; ++i) {
+        cin >> s[i];
+    }
+    map<char, char> f, g;
+    for (int i=0; i<26; ++i) {
+        f[x[i]] = 'a' + i;
+    }
+    for (int i=0; i<26; ++i) {
+        g['a' + i] = x[i];
+    }
+
+    for (int i=0; i<n; ++i) {
+        for (char& c : s[i]) c = f[c];
+    }
+
+    sort(s.begin(), s.end());
+    for (int i=0; i<n; ++i) {
+        for(char& c : s[i]) c = g[c];
+    }
+    for (int i=0; i<n; ++i) {
+        cout << s[i] << endl;
+    }
+    return 0;
+}
