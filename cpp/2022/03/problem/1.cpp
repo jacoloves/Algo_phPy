5114cc2b9caf9b6435d806078b2d3395f51cf0b4
jacoloves <5511068t@gmail.com>
2022-03-03 11:30:31 +0900

    220303

--- cpp/2022/03/problem/1.cpp
+++ cpp/2022/03/problem/1.cpp
@@ -22,7 +22,27 @@ typedef long long ll;
 using P = pair<int, int>;
 
 int main() {
-    int ele[] = {1, 2, 3, 4};    
+    int n;
+    cin >> n;
+
+    vector<pair<ll, ll>> p(n);
+    for (int i=0; i<n; ++i) {
+        cin >> p[i].first >> p[i].second;
+    }
+
+    int ans = 0;
+
+    for (int i=0; i<n; ++i) {
+        for (int j=i+1; j<n; ++j) {
+            for (int k=j+1; k<n; ++k) {
+                if ((p[j].first-p[i].first)*(p[k].second-p[i].second)-(p[k].first-p[i].first)*(p[j].second-p[i].second) != 0) {
+                    ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
