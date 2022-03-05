5f45c59ae84cdb952dd7bcc06823c04d9794aa9e
jacoloves <5511068t@gmail.com>
2022-03-05 22:17:23 +0900

    220305

---
+++ cpp/2022/03/contest/1.cpp
@@ -0,0 +1,42 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int a,b,c,x;
+    cin >> a >> b >> c >> x;
+
+    double ans = 1.0;
+
+    if (a < x) {
+        if (b < x) {
+            ans = 0.0;
+        } else {
+            int d = b - a;
+            ans = (double)c / d;
+        }
+    }
+
+    printf("%0.10f\n", ans);
+
+    return 0;
+}
