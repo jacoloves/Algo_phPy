8c59ad9488eda6777f093f7749cd4beb0a126fb9
jacoloves <5511068t@gmail.com>
2022-03-06 22:00:51 +0900

    220306

--- cpp/2022/03/contest/3.cpp
+++ cpp/2022/03/contest/3.cpp
@@ -1,66 +1,49 @@
-#include <iostream> // cout, endl, cin
-#include <string> // string, to_string, stoi
-#include <vector> // vector
-#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
-#include <utility> // pair, make_pair
-#include <tuple> // tuple, make_tuple
-#include <cstdint> // int64_t, int*_t
-#include <cstdio> // printf
-#include <map> // map
-#include <queue> // queue, priority_queue
-#include <set> // set
-#include <stack> // stack
-#include <deque> // deque
-#include <unordered_map> // unordered_map
-#include <unordered_set> // unordered_set
-#include <bitset> // bitset
-#include <cctype> // isupper, islower, isdigit, toupper, tolower
+    #include <iostream> // cout, endl, cin
+    #include <string> // string, to_string, stoi
+    #include <vector> // vector
+    #include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+    #include <utility> // pair, make_pair
+    #include <tuple> // tuple, make_tuple
+    #include <cstdint> // int64_t, int*_t
+    #include <cstdio> // printf
+    #include <map> // map
+    #include <queue> // queue, priority_queue
+    #include <set> // set
+    #include <stack> // stack
+    #include <deque> // deque
+    #include <unordered_map> // unordered_map
+    #include <unordered_set> // unordered_set
+    #include <bitset> // bitset
+    #include <cctype> // isupper, islower, isdigit, toupper, tolower
 
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
-using P = pair<int, int>;
+    using namespace std;
+    typedef long long ll;
+    #define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+    using P = pair<int, int>;
 
-const int mod = 998244353;
-
-int main() {
-    int n;
-    cin >> n;
-
-    ll ans = 0;
-    int tmp = 0;
-    // for (int h=1; h<=9; ++h) {
-    //     for (int i = 0; i<n; ++i) {
-    //         for (int j=1; j<=9; j++) {
-    //             if (i != 0) {
-    //                 if (tmp ) {
-    //                     ans *= 10;
-    //                     ans += j;
-    //                 } else {
-    //                     continue
-    //                 }
-    //             } else {
-    //                 ans += h;
-    //                 tmp = h;
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
-
-    for (int i=1; i<=9; ++i) {
-        for (int j=1; j<=9; ++j) {
-            for(int k=0; k<n; ++k) {
-                if (k != 0) {
-                    if (abs(tmp - j))
-                } else {
-                    ans += i;
-                    tmp = i;
-                    break;
-                }
-            }
-        }
+    const int MOD = 998244353;
+    int dp[1000005][10];
+    void add(int& x, int y) {
+        x += y;
+        x %= MOD;
     }
 
-    return 0;
-}
+    int main() {
+        int n;
+        cin >> n;
+
+        for(int d=1; d<=9; d++) dp[1][d] = 1;
+        for(int i=2; i<=n; i++) {
+            for(int d=1; d<=9; d++) {
+                if (d-1 >= 1) add(dp[i][d], dp[i-1][d-1]);
+                add(dp[i][d], dp[i-1][d]);
+                if (d+1 <= 9) add(dp[i][d], dp[i-1][d+1]);
+            }
+        }
+
+        int ans = 0;
+        for (int d=1; d<=9; d++) add(ans, dp[n][d]);
+        cout << ans << endl;
+
+        return 0;
+    }
