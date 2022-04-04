fb3545010b20a69e3a6b8ff6ca05c53855f81ee6
jacoloves <5511068t@gmail.com>
2022-04-04 21:00:02 +0900

    num full search 1

---
+++ cpp/2022/03/problem/31.cpp
@@ -0,0 +1,36 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (int i = 1; i <= n; ++i) {
+        if ((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0)) cnt++;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
