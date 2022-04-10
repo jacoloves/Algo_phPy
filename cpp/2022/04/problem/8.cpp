7a4f6c944b309c194103ec7a3d12cda18d00a06e
jacoloves <5511068t@gmail.com>
2022-04-10 14:07:22 +0900

    string search

---
+++ cpp/2022/04/problem/8.cpp
@@ -0,0 +1,42 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    string str;
+    char c;
+    cin >> str;
+    cin >> c;
+
+    bool flg = false;
+    for (int i=0; i<str.length(); ++i) {
+        if (str[i] == c) {
+            flg = true;
+            break;
+        }
+    }
+
+    if(flg) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
