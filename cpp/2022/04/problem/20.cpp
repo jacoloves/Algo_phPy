1aaf888bfe47a20c8f6e76e3e19e05da161049d4
jacoloves <5511068t@gmail.com>
2022-04-17 02:12:36 +0900

    220417

---
+++ cpp/2022/04/problem/20.cpp
@@ -0,0 +1,48 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> v(n);
+    vector<string> rev_v(n);
+
+    for (int i=0; i<n; ++i) {
+        cin >> v[i];
+    }
+
+    for (int i=0; i<n; ++i) {
+        rev_v[i] = v[i];
+    }
+
+    int cnt = 0;
+    for (int i=0; i<n; ++i) {
+        reverse(rev_v[i].begin(), rev_v[i].end());
+        if (v[i] == rev_v[i]) cnt++;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
