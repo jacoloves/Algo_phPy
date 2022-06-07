650b9d6a863efd0ed79bef385ea1b1df932f6b4e
jacoloves <5511068t@gmail.com>
2022-06-07 13:49:10 +0900

    220607

---
+++ cpp/2022/06/problem/4.cpp
@@ -0,0 +1,41 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <regex> // regex
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int n;
+    string str = "";
+
+    cin >> n;
+
+    int a = n % 10;
+    n /= 10;
+
+    int b = n % 10;
+
+    str = to_string(b) + to_string(a);
+
+    cout <<  str << endl; 
+
+    return 0;
+}
